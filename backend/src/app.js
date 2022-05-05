ee0cd35f496292bde2158553541c7e687aea3cee
Trong Nghia <52541823+asenta2812@users.noreply.github.com>
2022-05-05 14:09:36 +0700

    Add cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -31,7 +31,7 @@ app.use(
     crossOriginResourcePolicy: false,
   })
 );
-app.use(cors());
+app.use(cors({ origin: app.get('frontEndUrl'), credentials: true }));
 app.use(compress());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
